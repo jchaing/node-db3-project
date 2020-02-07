7bcb269bcbb077b6205a7eb3ed305d8de08828d5
john <john@spinningplates.org>
2020-02-06 17:47:05 -0800

    finished database methods

---
+++ schemes/scheme-model.js
@@ -0,0 +1,49 @@
+const db = require('../data/db-config.js');
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+  return db('schemes')
+    .where({ id })
+    .first();
+}
+
+function findSteps(id) {
+  return db('schemes as sc')
+    .join('steps as st', 'sc.id', 'st.scheme_id')
+    .select('st.id', 'sc.scheme_name', 'st.step_number', 'st.instructions')
+    .where({ scheme_id: id })
+    .orderBy('step_number');
+}
+
+function add(schemeData) {
+  return db('schemes').insert(schemeData);
+}
+
+function update(changes, id) {
+  return db('schemes')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db('schemes')
+    .where({ id })
+    .del();
+}
+
+function addStep(step, scheme_id) {
+
+}
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+  addStep
+};
